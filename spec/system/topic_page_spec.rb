c7641e22e73e1c6dd4da92e16c79aeb1354cabb7
Ivan Rabkesov <i.rabkesov@tezis-doc.ru>
2026-03-23 15:46:41 +0400

    fix(spec): fabricate post and visit with post_number to avoid TopicFromParams crash

--- spec/system/topic_page_spec.rb
+++ spec/system/topic_page_spec.rb
@@ -4,9 +4,10 @@ RSpec.describe "AI Summary in topic header theme" do
   let!(:theme) { upload_theme_component }
 
   let(:topic) { Fabricate(:topic) }
+  let!(:post) { Fabricate(:post, topic: topic) }
 
   it "renders the topic title wrapper the theme targets" do
-    visit("/t/#{topic.slug}/#{topic.id}")
+    visit("/t/#{topic.slug}/#{topic.id}/#{post.post_number}")
 
     expect(page).to have_css("#topic-title .title-wrapper h1")
   end
